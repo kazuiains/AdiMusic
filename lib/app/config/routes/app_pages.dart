4a9e1c0c80f5c1bdf0728a7a7d883d06b1037aee
kazuiains <kaisaryusuf@gmail.com>
2023-03-26 19:22:07 +0700

    refactor: remove page favorite

--- lib/app/config/routes/app_pages.dart
+++ lib/app/config/routes/app_pages.dart
@@ -1,10 +1,8 @@
 import 'package:get/get.dart';
 import 'package:musik/app/config/routes/app_routes.dart';
 import 'package:musik/presentation/get/detail/detail_binding.dart';
-import 'package:musik/presentation/get/favorite/favorite_binding.dart';
 import 'package:musik/presentation/get/home/home_binding.dart';
 import 'package:musik/presentation/ui/pages/detail/detail_page.dart';
-import 'package:musik/presentation/ui/pages/favorite/favorite_page.dart';
 import 'package:musik/presentation/ui/pages/home/home_page.dart';
 
 class AppPages {
@@ -18,11 +16,6 @@ class AppPages {
       name: AppRoutes.detail,
       page: () => DetailPage(),
       binding: DetailBinding(),
-    ),
-    GetPage(
-      name: AppRoutes.favorite,
-      page: () => FavoritePage(),
-      binding: FavoriteBinding(),
     ),
   ];
 }
--- lib/app/config/routes/app_routes.dart
+++ lib/app/config/routes/app_routes.dart
@@ -2,5 +2,4 @@ abstract class AppRoutes {
   static const String root = '/';
   static const String home = '/home';
   static const String detail = '/detail';
-  static const String favorite = '/favorite';
 }
